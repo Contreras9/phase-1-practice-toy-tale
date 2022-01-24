f06094a411fb16a370f26988edfb4e8b91ec5d7d
Hamzah Contreras <Hamzah_36@outlook.com>
2022-01-24 09:56:10 -0600

    commit tree 5792352dffc6c91eefdba3feb6d3de414b19f2e4

--- src/index.js
+++ src/index.js
@@ -13,3 +13,71 @@ document.addEventListener("DOMContentLoaded", () => {
     }
   });
 });
+
+
+fetch('http://localhost:3000/toys')
+.then(resp => resp.json())
+.then(toyData => {
+  toyData.forEach(toy => {
+    displayToy(toy)
+  })
+})
+function displayToy(toy) {
+    let div = document.getElementById('toy-collection')
+    let newDiv = document.createElement('div')
+    div.appendChild(newDiv)
+    newDiv.className = 'card'
+    let newH2 = document.createElement('h2')
+    newDiv.appendChild(newH2)
+    newH2.innerHTML = toy.name
+    let newImage = document.createElement('img')
+    newDiv.appendChild(newImage)
+    newImage.src = toy.image
+    newImage.className = 'toy-avatar'
+    let newp = document.createElement('p')
+    newDiv.appendChild(newp)
+    newp.innerHTML = toy.likes
+    let newButton = document.createElement('button')
+    newButton.addEventListener('click', () => {
+      fetch(`http://localhost:3000/toys/${toy.id}`, {
+        method: 'PATCH',
+        headers:{
+         "Content-Type": "application/json",
+         Accept: "application/json"
+        },
+        body: JSON.stringify({
+          "likes": ++toy.likes
+        })
+      })
+      .then(resp => resp.json())
+      .then(data => {
+        newp.innerHTML = data.likes
+      })
+    })
+    newDiv.appendChild(newButton)
+    newButton.className = 'like-btn'
+    newButton.id = toy.id
+    newButton.innerHTML = 'Like'
+}
+let newForm = document.getElementsByClassName('add-toy-form')[0]
+newForm.addEventListener('submit', e => {
+  e.preventDefault()
+  let formName = document.getElementsByName('name')[0]
+  let formImg = document.getElementsByName('image')[0]
+  fetch("http://localhost:3000/toys", {
+    method: 'POST',
+    headers:{
+      "Content-Type": "application/json",
+      Accept: "application/json"
+    },
+    body: JSON.stringify({
+      "name": formName.value,
+      "image": formImg.value,
+      "likes": 0
+    })
+  })
+  .then(resp => resp.json())
+  .then(toy => {
+    displayToy(toy)
+  })
+})
